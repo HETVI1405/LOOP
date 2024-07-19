973d65d5f2cb3d0cb4488096c0b13143bc86b250
HETVI1405 <phetvi1405@gmail.com>
2024-07-19 12:21:45 +0530

    Add files via upload

---
+++ loop1toN.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int x=1,N;
+	
+	printf("enter N vlue:");
+	scanf("%d",&N);
+	
+	while(x<=N)
+	{
+		printf("%d\n",x);
+		x++;
+	}
+}
---
+++ odd num.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+
+ void main()
+ {
+ 	int i=1,N;
+ 	
+ 	printf("enter vlue N:");
+ 	scanf("%d",&N);
+ 	
+ 	
+ 	while(N>=i)
+	 
+ 	{
+ 		
+ 		if(N%2==1)
+ 		{
+ 			printf("%d\t",N);
+ 			
+		 }
+		 N--;
+	 }
+ 	
+ }
